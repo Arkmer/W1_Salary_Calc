69f02827655baa4566ab42ce6b76ffcc39c33816
Ryan Udelhoven <Ryan@Udelhoven.net>
2018-02-03 15:45:36 -0600

    rank now changes from input moniker to actual rank.

--- salaryCalc.js
+++ salaryCalc.js
@@ -4,16 +4,65 @@ function listeners(){
   $('#ipSoldier').on('click', fColloectData);
 }
 
+var soldierDisp = [];
+
 function fColloectData(){
   var Soldier = {
     firstName: $('#ipNameFirst').val(),
     lastName: $('#ipNameLast').val(),
-    rankGrade: $('#rankGrade').val(),
+    rankGrade: rankTerp(),
     years: $('#ipYears').val(),
     mos: $('#ipMOS').val(),
     dodid: $('#ipDoDID').val()
   }
-  console.log(Soldier);
+  soldierDisp.push(Soldier);
+  console.log(soldierDisp);
+}
+
+function rankTerp(){
+  var lRankMon;
+  switch (parseInt($('#rankGrade').val())) {
+    case 1:
+        lRankMon = 'PVT';
+        break;
+    case 2:
+        lRankMon = 'PV2';
+        break;
+    case 3:
+        lRankMon = 'PFC';
+        break;
+    case 4:
+        lRankMon = 'SPC';
+        break;
+    case 5:
+        lRankMon = 'CPL';
+        break;
+    case 6:
+        lRankMon = 'SGT';
+        break;
+    case 7:
+        lRankMon = 'SSG';
+        break;
+    case 8:
+        lRankMon = 'SFC';
+        break;
+    case 9:
+        lRankMon = '1SG';
+        break;
+    case 10:
+        lRankMon = 'MSG';
+        break;
+    case 11:
+        lRankMon = 'SGM';
+        break;
+    case 12:
+        lRankMon = 'CSM';
+        break;
+    case 13:
+        lRankMon = 'SMA';
+  }
+  console.log(lRankMon);
+  return lRankMon;
 }
 
 // Delete button that removes an employee
